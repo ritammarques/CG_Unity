fbf5adae3c8db438a68c1dcbb47154e73b639518
ritammarques <50811505+ritammarques@users.noreply.github.com>
2020-07-20 17:44:24 +0100

    1Commit

---
+++ Assets/CameraFolllow.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+//Referencia_tirado de: /*https://www.youtube.com/watch?v=MFQhpwc6cKE */
+public class CameraFolllow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
---
+++ Assets/Scenes/Animações.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+
+public class Animações : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator animator;
+    int jumpHash = Animator.StringToHash("Jump");
+    int rotateRightHash = Animator.StringToHash("RotateRight");
+    int rotateLeftHash = Animator.StringToHash("RotateLeft");
+
+   
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        //andar e correr
+        float move = Input.GetAxis ("Vertical");
+        animator.SetFloat("Speed", move);
+
+
+        //saltar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger(jumpHash);
+        }
+        //Rodar Direita
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            animator.SetTrigger(rotateRightHash);
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            animator.SetTrigger(rotateLeftHash);
+        }
+      
+    }
+}
